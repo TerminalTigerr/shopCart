8b3aa095a6a980ee9c300022ad81c20e2dce9c51
BitHero <nuhumusah874@gmail.com>
2023-07-16 15:38:32 +0000

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,13 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
-    },
+
     extend: {
       colors: {
         darkGreen: 'rgb(26, 93, 26)',
-        deepYellow: 'rgb(241, 201, 59)',
-        deepYellowLigt: 'rgb(251, 216, 93)',
-        deepYellowSupLight: 'rgb(250, 227, 146)'
+        yellow: 'rgb(241, 201, 59)',
+        yellowLigt: 'rgb(251, 216, 93)',
+        yellowSupLight: 'rgb(250, 227, 146)'
       }
     },
   },
